26237e7aac94527bf63001521154ae30d94188a9
xadrianzetx <xadrianzetx@gmail.com>
2022-08-18 21:30:59 +0200

    Add docstring to `process` method

--- optuna_distributed/messages/base.py
+++ optuna_distributed/messages/base.py
@@ -17,4 +17,18 @@ class Message(ABC):
 
     @abc.abstractmethod
     def process(self, study: "Study", manager: "OptimizationManager") -> None:
+        """Process a message data with context available in main process.
+
+        Concrete implementations of this method should contain operations that
+        worker wants to execute using resources available only to the main process.
+        This means stuff like hyperparameter suggestions, prune commands and general
+        data passing.
+
+        Args:
+            study:
+                An instance of Optuna study.
+            manager:
+                :class:`~optuna_distributed.managers.Manager` providing additional
+                execution context.
+        """
         raise NotImplementedError
