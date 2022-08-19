a3284fd4f4969e9f0453e831733c11be68a3b11d
xadrianzetx <xadrianzetx@gmail.com>
2022-08-19 20:59:57 +0200

    Implemented `Pipe`

--- optuna_distributed/ipc/__init__.py
+++ optuna_distributed/ipc/__init__.py
@@ -1,5 +1,6 @@
 from optuna_distributed.ipc.base import IPCPrimitive
+from optuna_distributed.ipc.pipe import Pipe
 from optuna_distributed.ipc.queue import Queue
 
 
-__all__ = ["IPCPrimitive", "Queue"]
+__all__ = ["IPCPrimitive", "Queue", "Pipe"]
---
+++ optuna_distributed/ipc/pipe.py
@@ -0,0 +1,33 @@
+from multiprocessing.connection import Connection
+from typing import TYPE_CHECKING
+
+from optuna_distributed.ipc import IPCPrimitive
+
+
+if TYPE_CHECKING:
+    from optuna_distributed.messages import Message
+
+
+class Pipe(IPCPrimitive):
+    """IPC primitive based on multiprocessing Pipe.
+
+    Forms a thin layer of abstraction over one end of multiprocessing
+    pipe connection, with get/put semantics.
+
+    Args:
+        connection:
+            One of the ends of multiprocessing pipe.
+            https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Pipe
+    """
+
+    def __init__(self, connection: Connection) -> None:
+        self._connection = connection
+
+    def get(self) -> "Message":
+        return self._connection.recv()
+
+    def put(self, message: "Message") -> None:
+        return self._connection.send(message)
+
+    def close(self) -> None:
+        return self._connection.close()
