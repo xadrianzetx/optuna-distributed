b34a9f388b705d779e93f7abbca307ca478e8b3b
xadrianzetx <xadrianzetx@gmail.com>
2022-08-21 10:12:17 +0200

    Add missing study stubs

--- optuna_distributed/study.py
+++ optuna_distributed/study.py
@@ -1,15 +1,22 @@
 import sys
+from typing import Any
 from typing import Callable
+from typing import Container
+from typing import Dict
+from typing import Iterable
 from typing import List
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
 from typing import Type
+from typing import TYPE_CHECKING
 from typing import Union
 
 from dask.distributed import Client
+from optuna.distributions import BaseDistribution
 from optuna.exceptions import TrialPruned
 from optuna.study import Study
+from optuna.study import StudyDirection
 from optuna.trial import FrozenTrial
 from optuna.trial import Trial
 from optuna.trial import TrialState
@@ -19,10 +26,14 @@ from optuna_distributed.managers import DistributedOptimizationManager
 from optuna_distributed.managers import LocalOptimizationManager
 from optuna_distributed.messages import CompletedMessage
 from optuna_distributed.messages import FailedMessage
-from optuna_distributed.messages import ResponseMessage
 from optuna_distributed.messages import PrunedMessage
 from optuna_distributed.messages import RepeatedTrialMessage
+from optuna_distributed.messages import ResponseMessage
 from optuna_distributed.trial import DistributedTrial
+
+
+if TYPE_CHECKING:
+    import pandas as pd
 
 
 ObjectiveFuncType = Callable[[Trial], Union[float, Sequence[float]]]
@@ -44,8 +55,45 @@ class DistributedStudy:
         self._client = client
 
     @property
+    def best_params(self) -> Dict[str, Any]:
+        pass
+
+    @property
+    def best_value(self) -> float:
+        pass
+
+    @property
     def best_trial(self) -> FrozenTrial:
         return self._study.best_trial
+
+    @property
+    def best_trials(self) -> List[FrozenTrial]:
+        pass
+
+    @property
+    def direction(self) -> StudyDirection:
+        pass
+
+    @property
+    def directions(self) -> List[StudyDirection]:
+        pass
+
+    @property
+    def trials(self) -> List[FrozenTrial]:
+        pass
+
+    @property
+    def user_attrs(self) -> Dict[str, Any]:
+        pass
+
+    @property
+    def system_attrs(self) -> Dict[str, Any]:
+        pass
+
+    def get_trials(
+        self, deepcopy: bool = True, states: Optional[Container[TrialState]] = None
+    ) -> List[FrozenTrial]:
+        pass
 
     def optimize(
         self,
@@ -106,6 +154,58 @@ class DistributedStudy:
 
         finally:
             self._study._storage.remove_session()
+
+    def ask(self, fixed_distributions: Optional[Dict[str, BaseDistribution]] = None) -> Trial:
+        pass
+
+    def tell(
+        self,
+        trial: Union[Trial, int],
+        values: Optional[Union[float, Sequence[float]]] = None,
+        state: Optional[TrialState] = None,
+        skip_if_finished: bool = False,
+    ) -> FrozenTrial:
+        pass
+
+    def set_user_attr(self, key: str, value: Any) -> None:
+        pass
+
+    def set_system_attr(self, key: str, value: Any) -> None:
+        pass
+
+    def trials_dataframe(
+        self,
+        attrs: Tuple[str, ...] = (
+            "number",
+            "value",
+            "datetime_start",
+            "datetime_complete",
+            "duration",
+            "params",
+            "user_attrs",
+            "system_attrs",
+            "state",
+        ),
+        multi_index: bool = False,
+    ) -> "pd.DataFrame":
+        pass
+
+    def stop(self) -> None:
+        pass
+
+    def enqueue_trial(
+        self,
+        params: Dict[str, Any],
+        user_attrs: Optional[Dict[str, Any]] = None,
+        skip_if_exists: bool = False,
+    ) -> None:
+        pass
+
+    def add_trial(self, trial: FrozenTrial) -> None:
+        pass
+
+    def add_trials(self, trials: Iterable[FrozenTrial]) -> None:
+        pass
 
 
 def from_optuna_study(study: Study) -> DistributedStudy:
