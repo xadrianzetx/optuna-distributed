5422ac0213632b5f9065f30bea001a2b43d1e745
xadrianzetx <xadrianzetx@gmail.com>
2022-08-22 21:01:57 +0200

    Add test coverage to heartbeat, pruned and property messages

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,12 +1,17 @@
 from unittest.mock import MagicMock
 
+from optuna.exceptions import TrialPruned
 from optuna.study import Study
 from optuna.trial import TrialState
 import pytest
 
 from optuna_distributed.messages import CompletedMessage
 from optuna_distributed.messages import FailedMessage
+from optuna_distributed.messages import HeartbeatMessage
+from optuna_distributed.messages import PrunedMessage
 from optuna_distributed.messages import ShouldPruneMessage
+from optuna_distributed.messages import TrialProperty
+from optuna_distributed.messages import TrialPropertyMessage
 
 
 def test_completed_with_correct_value(study: Study, optimization_manager: MagicMock) -> None:
@@ -27,6 +32,15 @@ def test_completed_with_incorrect_values(study: Study, optimization_manager: Mag
     assert optimization_manager.register_trial_exit.called_once_with(0)
 
 
+def test_pruned(study: Study, optimization_manager: MagicMock) -> None:
+    msg = PrunedMessage(0, TrialPruned())
+    assert msg.closing
+    msg.process(study, optimization_manager)
+    assert optimization_manager.register_trial_exit.called_once_with(0)
+    trial = study.get_trials(deepcopy=False, states=(TrialState.PRUNED,))
+    assert len(trial) == 1
+
+
 def test_failed(study: Study, optimization_manager: MagicMock) -> None:
     exc = ValueError("foo")
     msg = FailedMessage(0, exc, exc_info=MagicMock())
@@ -37,6 +51,32 @@ def test_failed(study: Study, optimization_manager: MagicMock) -> None:
     assert optimization_manager.register_trial_exit.called_once_with(0)
     trial = study.get_trials(deepcopy=False, states=(TrialState.FAIL,))
     assert len(trial) == 1
+
+
+def test_heartbeat() -> None:
+    msg = HeartbeatMessage()
+    assert not msg.closing
+
+
+@pytest.mark.parametrize(
+    "name,property",
+    [
+        ("params", TrialProperty.PARAMS),
+        ("distributions", TrialProperty.DISTRIBUTIONS),
+        ("user_attrs", TrialProperty.USER_ATTRS),
+        ("system_attrs", TrialProperty.SYSTEM_ATTRS),
+        ("datetime_start", TrialProperty.DATETIME_START),
+        ("number", TrialProperty.NUMBER),
+    ],
+)
+def test_trial_property(
+    study: Study, optimization_manager: MagicMock, name: str, property: TrialProperty
+) -> None:
+    msg = TrialPropertyMessage(0, property)
+    assert not msg.closing
+    msg.process(study, optimization_manager)
+    expected = getattr(study.get_trials(deepcopy=False)[0], name)
+    assert optimization_manager.get_connection.return_value_called_once_with(expected)
 
 
 def test_should_prune(study: Study, optimization_manager: MagicMock) -> None:
