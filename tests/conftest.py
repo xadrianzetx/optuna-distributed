cca5cb7970985239109284bf847991c96ec4acf0
xadrianzetx <xadrianzetx@gmail.com>
2022-08-23 18:11:52 +0200

    Add common dask test client as a fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
+from dask.distributed import Client
 import optuna
 import pytest
+
+
+_test_client = Client(n_workers=1, threads_per_worker=1)
+
+
+@pytest.fixture
+def client() -> Client:
+    return _test_client
 
 
 @pytest.fixture
