90574f707ac51a2b51da245675ec23747ca91ce5
xadrianzetx <xadrianzetx@gmail.com>
2022-10-15 15:50:04 +0200

    Make optimization managers responsible for wrapping objective function
    This allows us to have backend specific implementations of objective
    function wrappers.

--- optuna_distributed/managers/__init__.py
+++ optuna_distributed/managers/__init__.py
@@ -1,6 +1,14 @@
+from optuna_distributed.managers.base import DistributableFuncType
+from optuna_distributed.managers.base import ObjectiveFuncType
 from optuna_distributed.managers.base import OptimizationManager
 from optuna_distributed.managers.distributed import DistributedOptimizationManager
 from optuna_distributed.managers.local import LocalOptimizationManager
 
 
-__all__ = ["OptimizationManager", "DistributedOptimizationManager", "LocalOptimizationManager"]
+__all__ = [
+    "OptimizationManager",
+    "DistributedOptimizationManager",
+    "LocalOptimizationManager",
+    "DistributableFuncType",
+    "ObjectiveFuncType",
+]
