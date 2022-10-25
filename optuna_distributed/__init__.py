e6f17804daf8f789e726eb3fda9675e7eceb7182
xadrianzetx <xadrianzetx@gmail.com>
2022-10-25 18:39:16 +0200

    Switch log handler to `RichHandler`

--- optuna_distributed/__init__.py
+++ optuna_distributed/__init__.py
@@ -4,12 +4,10 @@ from optuna_distributed.study import from_study
 def _setup_logger() -> None:
     import logging
 
-    import colorlog
+    from rich.logging import RichHandler
 
-    fmt = colorlog.ColoredFormatter(
-        "%(log_color)s[%(levelname)1.1s %(asctime)s]%(reset)s %(message)s"
-    )
-    handler = logging.StreamHandler()
+    handler = RichHandler(show_path=False)
+    fmt = logging.Formatter(fmt="%(message)s", datefmt="[%X]")
     handler.setFormatter(fmt)
     root_logger = logging.getLogger(__name__)
     root_logger.addHandler(handler)
