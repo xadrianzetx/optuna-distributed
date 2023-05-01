a4c469601de586e0d149ca25979a09b29c6fa55a
xadrianzetx <xadrianzetx@gmail.com>
2023-05-01 13:23:47 +0200

    Move logger setup to own module, introduce helpers

--- optuna_distributed/__init__.py
+++ optuna_distributed/__init__.py
@@ -1,19 +1,6 @@
+from optuna_distributed import config
 from optuna_distributed.study import from_study
 
 
-def _setup_logger() -> None:
-    import logging
-
-    from rich.logging import RichHandler
-
-    handler = RichHandler(show_path=False)
-    fmt = logging.Formatter(fmt="%(message)s", datefmt="[%X]")
-    handler.setFormatter(fmt)
-    root_logger = logging.getLogger(__name__)
-    root_logger.addHandler(handler)
-    root_logger.setLevel(logging.INFO)
-
-
-_setup_logger()
 __version__ = "0.4.0"
-__all__ = ["from_study"]
+__all__ = ["from_study", "config"]
